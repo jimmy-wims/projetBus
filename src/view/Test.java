af3fa42e7db209bfd271d162ff6aa4a4fb2e9459
jimmy-wims <49988914+jimmy-wims@users.noreply.github.com>
2020-06-19 15:21:57 +0200

    Add files via upload

--- src/view/Test.java
+++ src/view/Test.java
@@ -1,20 +1,56 @@
 package view;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 
+import dao.ArretDAOJSON;
 import dao.BusDAOJSON;
 import dao.DAO;
 import dao.LigneDAOJSON;
-import element.Bus;
-import element.LigneDeBus;
+import modele.Arret;
+import modele.Bus;
+import modele.LigneDeBus;
+import modele.ReseauFacade;
 
 public class Test {
 
 	public static void main(String[] args) {
 		DAO<Bus> bus = BusDAOJSON.getInstance();
 		DAO<LigneDeBus> ligne = LigneDAOJSON.getInstance();
-		bus.create(new Bus(42));
-		ligne.create(new LigneDeBus("uneLigne",null));
+		DAO<Arret> daoArret = ArretDAOJSON.getInstance();
+		
+		
+		//ligne.create(new LigneDeBus("ligneTest"));
+		
+		ReseauFacade facade = ReseauFacade.getInstance();
+		facade.getLesLignes();
+		facade.getLesBus();
+		
+		ArrayList<String> lesLignes= new ArrayList<String>();
+		lesLignes.add("uneLigne"); lesLignes.add("listeLigne");
+		//Arret arret = new Arret("arret1", "ici", lesLignes);
+		ArrayList<LigneDeBus> listeLigne = new ArrayList<LigneDeBus>();
+		listeLigne.add(new LigneDeBus("uneLigne"));
+		listeLigne.add(new LigneDeBus("listeLigne"));
+		facade.ajouterArret("arret1", "ici", lesLignes, listeLigne);
+		for(String s : lesLignes)
+			facade.enregisterLigne(s);
+		//bus.create(new Bus(34));
+		
+		/*ArrayList<String> lesLignes= new ArrayList<String>();
+		lesLignes.add("ligneTest");
+		Arret arret = new Arret("testCreation", "Paris", lesLignes);
+		
+		daoArret.delete(arret);
+		ArrayList<String> lesLignes= new ArrayList<String>();
+		lesLignes.add("ligneTest");
+		
+		daoArret.create(arret);
+		lesLignes.add("uneLigne");
+		arret.setListNomLigne(lesLignes);
+		daoArret.update(arret);*/
+		
+		//daoArret.saveAll("testCreation")
 	}
 
 }
